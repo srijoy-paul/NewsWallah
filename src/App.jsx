e5a907e3dc7b9f64831c2ee41993f7f8c648b112
Srijoy Paul <srijoykumar1507@gmail.com>
2023-01-24 23:05:39 +0530

    Initial React-app setup with vite config

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React, { Component, useState } from 'react'
+import './App.css'
+
+class App extends Component {
+  render() {
+    return (<div>
+      <h1>React app with vite instead of CRA</h1>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Distinctio quidem commodi dolorem harum esse, repellendus error non voluptates minima numquam natus minus ipsa in voluptas repudiandae animi? Accusamus, quidem laborum.
+        Modi, recusandae temporibus. Atque sunt exercitationem cumque iste voluptatum itaque sit numquam voluptates optio. Voluptas exercitationem maiores reprehenderit voluptatibus delectus, impedit magnam perspiciatis soluta in ut aliquam laudantium, consectetur quos.
+        Ipsam officia, molestias sit harum quasi, ea vitae dicta eius autem nihil distinctio fugiat corporis amet neque repudiandae excepturi iusto aspernatur doloremque enim ratione consequuntur magnam tempore quas optio? Dolore?
+        Eligendi natus explicabo suscipit dolore asperiores veniam numquam quod totam, temporibus dolor, veritatis consequuntur nihil enim inventore laudantium, nostrum non rem eum quo cupiditate vitae illum quibusdam. Temporibus, ratione ipsa?
+        Est dignissimos nihil repudiandae vitae nobis quos nam porro ea obcaecati, odit ipsam dolor excepturi quod saepe assumenda molestiae repellendus. Consectetur, ea. Dolor tenetur maiores expedita a unde, nesciunt eveniet.
+        Repudiandae, fuga incidunt voluptatibus hic ipsa ex quasi. Repudiandae enim iure sapiente cumque laboriosam quasi libero sequi a adipisci eveniet reiciendis vitae minima earum doloribus optio, voluptas quisquam qui provident!
+        Soluta velit dolorem aliquid ipsam autem accusantium accusamus, officia voluptatem distinctio sequi nesciunt quae exercitationem quasi porro hic, a delectus. Dicta maxime, ex ullam a explicabo quia voluptatum eveniet asperiores?
+        Ut consequuntur veniam molestias, earum ea repellendus adipisci alias laboriosam dignissimos optio, eum fugiat id laborum vel assumenda quas facere reiciendis non, ad ratione iure praesentium placeat commodi quia? Itaque.
+        Vitae veniam rem labore, nemo iste ipsam sit eius impedit, enim culpa maxime sint earum dolor. Iste eum, saepe corrupti id nesciunt minima, illo labore, odio facere eius placeat eveniet.
+        Adipisci culpa cum ex amet dignissimos fuga, id inventore quis rem vitae in eaque officiis! Illum perferendis ipsum earum doloribus deleniti repudiandae quibusdam fuga aspernatur porro placeat non, est officia.
+        Nulla, aspernatur deleniti. Eveniet, est porro id sint facilis harum consequuntur ducimus soluta iure quasi facere dolorum eligendi velit quas deserunt praesentium labore voluptatum a? Voluptatibus necessitatibus iusto repellendus voluptatem?
+        Voluptates, eveniet. Repellat quis magni eligendi. Quaerat consequatur aut, possimus non saepe deleniti, quos ea cumque, odit deserunt delectus doloribus consequuntur eaque optio harum? Vitae repellat eos cupiditate at et!</p>
+    </div>);
+  }
+}
+
+export default App;
