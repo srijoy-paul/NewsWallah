3914ba3cd43ea2b680a603f1d57760676bdcbd9e
Srijoy Paul <srijoykumar1507@gmail.com>
2023-01-26 23:21:21 +0530

    ComponentdidMount using useEffect, pagination

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ export default function Navbar() {
 
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
             <div className="container-fluid">
-                <a className="navbar-brand" href="#">DecentNews</a>
+                <a className="navbar-brand" href="#">NewsWallah</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
